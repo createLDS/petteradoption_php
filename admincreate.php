3b19935ab3b3e338932e0b14fc9943d4950c9c71
Sierra <32525359+sierrabcit@users.noreply.github.com>
2019-03-03 12:46:10 -0800

    updating files

--- admincreate.php
+++ admincreate.php
@@ -1,11 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Origin: no-cors');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
-  $conn = new PDO("mysql:cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname:ppg1bdg8coul7ed1","kpxp96k9j2q1anmp","er6s0s73za50ig2e");
-  //$conn = new PDO("mysql:host=localhost;dbname=petteradoption", "root", "root");
+  $conn = new PDO("mysql:host=localhost;dbname=petteradoption", "root", "root");
+  echo "Conneted!"
 } catch (PDOException $e) {
   echo "Error".$e->getMessage();
 }
@@ -17,7 +13,7 @@ $adminUsername = $_POST['adminUsername'];
 $adminPassword = $_POST['adminPassword'];
 
 //function to insert users into db
-$query = "INSERT INTO admin (adminFirstName,adminLastName,adminEmail,adminUsername,adminPassword) VALUES ('$adminFirstName','$adminLastName','$adminEmail','$adminUsername','$adminPassword')";
+$query = "INSERT INTO users (adminFirstName,adminLastName,adminEmail,adminUsername,adminPassword) VALUES ('$adminFirstName','$adminLastName','$adminEmail','$adminUsername','$adminPassword')";
 
 
 $result = $conn->query($query);
--- upload.php
+++ upload.php
@@ -1,10 +1,7 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
-  $conn = new PDO("mysql:cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname:ppg1bdg8coul7ed1","kpxp96k9j2q1anmp","er6s0s73za50ig2e");
-  //$conn = new PDO("mysql:host=localhost;dbname=petteradoption", "root", "root");
+  $conn = new PDO("mysql:host=localhost;dbname=petteradoption", "root", "root");
+  echo "Conneted!"
 } catch (PDOException $e) {
   echo "Error".$e->getMessage();
 }
@@ -16,25 +13,21 @@ $petDOB = $_POST['petDOB'];
 $petSpecies = $_POST['petSpecies'];
 $petPhotos = $_POST['petPhotos']; //must be in img folder
 $petadminID = $_POST['petadminID'];
-$petStatus = $_POST['petStatus'];
-
+]
 
 //function to insert users into db
-$query = "INSERT INTO pets (petName, petGender, petDOB, petSpecies, petPhotos, petadminID, petStatus) VALUES ('$petName','$petGender','$petDOB','$petSpecies','$petPhotos','$petadminID','$petStatus')";
+$query = "INSERT INTO pets (petName, petGender, petDOB, petSpecies, petPhotos, petadminID) VALUES ('$petName','$petGender','$petDOB','$petSpecies','$petPhotos','$petadminID')";
 
 
 $result = $conn->query($query);
 
 if($result){
   $id = $conn->lastInsertId();
-  //if worked
- // echo "Your upload was successful";
   echo json_encode(array(
     "status"=>true,
     "id"=>$id
   ));
 } else {
-  //echo "Sorry, your upload failed";
   echo json_encode(false);
 }
 
