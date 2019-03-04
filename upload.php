b5722ff51029f15f8c03c538b4bf677dccec9f29
zouyi <zouyi@live.ca>
2019-03-04 00:24:58 -0800

    more environment variable

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-$url = getenv('JAWSDB_URL');
+$url = getenv('mysql://kpxp96k9j2q1anmp:er6s0s73za50ig2e@cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ppg1bdg8coul7ed1');
 $dbparts = parse_url($url);
 
 $hostname = $dbparts['cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
