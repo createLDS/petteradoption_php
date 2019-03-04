dd3a04ef8192c7279827be6a86dfdb44451834d4
zouyi <zouyi@live.ca>
2019-03-03 23:58:25 -0800

    update pdo info to so that it can be connected remotely

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $conn = new PDO("mysql:host=localhost;dbname=petteradoption", "root", "root");
+  $conn = new PDO("mysql:host=cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ppg1bdg8coul7ed1", "kpxp96k9j2q1anmp", "er6s0s73za50ig2e");
   echo "Conneted!"
 } catch (PDOException $e) {
   echo "Error".$e->getMessage();
