cb00052d77c9016d479813f97d51bf3d13c6bf23
zouyi <zouyi@live.ca>
2019-03-04 00:07:41 -0800

    connect to JAWS

--- upload.php
+++ upload.php
@@ -1,6 +1,14 @@
 <?php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$username = $dbparts['kpxp96k9j2q1anmp'];
+$password = $dbparts['er6s0s73za50ig2e'];
+$database = ltrim($dbparts['ppg1bdg8coul7ed1'],'/');
+
 try {
-  $conn = new PDO("mysql:host=cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ppg1bdg8coul7ed1", "kpxp96k9j2q1anmp", "er6s0s73za50ig2e");
+	$conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
   echo "Conneted!"
 } catch (PDOException $e) {
   echo "Error".$e->getMessage();
